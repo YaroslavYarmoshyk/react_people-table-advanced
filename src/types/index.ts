186fcaa7222033e3fe8dc67b3d67f64a0d9efa64
Yaroslav <yarmoshyk96@gmail.com>
2025-08-10 23:03:00 +0300

    Remove unused code

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,8 +2,3 @@ export * from './Person';
 
 export type SortField = 'name' | 'sex' | 'born' | 'died';
 export type SortOrder = 'asc' | 'desc';
-
-export interface TableSortParams {
-  sort: SortField | null;
-  order: SortOrder | null;
-}
